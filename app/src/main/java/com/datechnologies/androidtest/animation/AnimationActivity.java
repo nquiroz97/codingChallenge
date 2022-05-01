4c4882f719d41082107d7e1328a7c59cc45aafa1
Neri Quiroz <68469197+nquiroz97@users.noreply.github.com>
2022-05-01 11:13:17 -0400

    functionality to drag image around

--- app/src/main/java/com/datechnologies/androidtest/animation/AnimationActivity.java
+++ app/src/main/java/com/datechnologies/androidtest/animation/AnimationActivity.java
@@ -1,10 +1,25 @@
 package com.datechnologies.androidtest.animation;
 
+import android.annotation.SuppressLint;
+import android.content.ClipData;
+import android.content.ClipDescription;
 import android.content.Context;
 import android.content.Intent;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.DragEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.datechnologies.androidtest.MainActivity;
 import com.datechnologies.androidtest.R;
@@ -19,6 +34,10 @@ public class AnimationActivity extends AppCompatActivity {
     //==============================================================================================
     // Class Properties
     //==============================================================================================
+    private ImageView imageView;
+    private android.widget.RelativeLayout.LayoutParams layoutParams;
+    private float startingX,  startingY;
+
 
     //==============================================================================================
     // Static Class Methods
@@ -34,6 +53,7 @@ public class AnimationActivity extends AppCompatActivity {
     // Lifecycle Methods
     //==============================================================================================
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,13 +65,45 @@ public class AnimationActivity extends AppCompatActivity {
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setDisplayShowHomeEnabled(true);
 
+        imageView = findViewById(R.id.draggableImage);
+
+        imageView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch (motionEvent.getActionMasked()) {
+                    case MotionEvent.ACTION_DOWN: {
+                        // Remember where we started (for dragging)
+                        startingX = motionEvent.getX();
+                        startingY = motionEvent.getY();
+                        break;
+                    }
+
+                    case MotionEvent.ACTION_MOVE: {
+                        final float movedX = motionEvent.getX();
+                        final float movedY = motionEvent.getY();
+
+                        // Calculate the distance moved
+                        final float distanceX = movedX - startingX;
+                        final float distanceY = movedY - startingY;
+
+                        imageView.setX(imageView.getX() + distanceX);
+                        imageView.setY(imageView.getY() + distanceY);
+
+                        view.invalidate();
+                        break;
+                    }
+                }
+                return true;
+            }
+        });
+
         // DONE: Make the UI look like it does in the mock-up. Allow for horizontal screen rotation.
         // DONE: Add a ripple effect when the buttons are clicked
 
         // TODO: When the fade button is clicked, you must animate the D & A Technologies logo.
         // TODO: It should fade from 100% alpha to 0% alpha, and then from 0% alpha to 100% alpha
 
-        // TODO: The user should be able to touch and drag the D & A Technologies logo around the screen.
+        // DONE: The user should be able to touch and drag the D & A Technologies logo around the screen.
 
         // TODO: Add a bonus to make yourself stick out. Music, color, fireworks, explosions!!!
     }
